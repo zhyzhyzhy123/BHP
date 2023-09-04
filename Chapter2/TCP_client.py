75ab1dd4486e1d45f2a774ae2df4741577e7a405
zhy <472739561@qq.com>
2023-09-04 11:00:58 -0400

    Commit for previous file

---
+++ Chapter2/TCP_client.py
@@ -0,0 +1,17 @@
+import socket
+
+target_host = "0.0.0.0"
+target_port = 9998
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((target_host, target_port))
+
+client.send(b"FUCK YOU DUMP ASS")
+
+response = client.recv(4096)
+print(response.decode())
+client.close()
+
+
+
